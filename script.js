a241e72deacd281d6bcaaddfb0a8c4292e51a003
ponjae <>
2021-05-17 16:29:08 +0200

    minor fix

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const completeBtn = document.getElementById('complete-button');
 
 let countdownTitle = '';
 let countdownDate = '';
-let countdownValue = Date;
+let countdownValue = Date();
 let countdownActive;
 let savedCountdown;
 
