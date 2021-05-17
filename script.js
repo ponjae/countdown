202bf14823dd2bba0ae83d337463b40bcecedaec
ponjae <>
2021-05-17 15:23:59 +0200

    Now able to get the title and date from the ui

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdown-form');
 const dateElem = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
+
 const today = new Date().toISOString().split('T')[0]; // Get todays date
 dateElem.setAttribute('min', today);
+
+function updateCountdown(e) {
+    e.preventDefault();
+    console.log(e);
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+
+countdownForm.addEventListener('submit', updateCountdown);
+
